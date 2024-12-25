219d5efe5f26eb060704798a38d6064518ff98e8
saideeWD <mdsaideehasan@gmail.com>
2024-12-25 21:14:55 +0600

    open modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,30 @@
-import Banner from "../components/Banner"
-import Map from "../components/Map"
-
-import FeaturedRooms from "../components/Rooms"
+import React, { useState, useEffect } from "react";
+import Banner from "../components/Banner";
+import Map from "../components/Map";
+import FeaturedRooms from "../components/Rooms";
+import SpecialOffersModal from "../components/SpecialOffersModal";
 
 function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    // Show the modal when the component mounts
+    setIsModalOpen(true);
+  }, []);
+
   return (
     <div className="container mx-auto">
-    <Banner></Banner>
-    <Map/>
-    <FeaturedRooms/>
-   
+      {/* Special Offers Modal */}
+      {isModalOpen && (
+        <SpecialOffersModal onClose={() => setIsModalOpen(false)} />
+      )}
+
+      <Banner />
+      <Map />
+      <FeaturedRooms />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
